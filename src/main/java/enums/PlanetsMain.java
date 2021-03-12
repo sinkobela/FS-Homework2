a8cfdad9dd68580104076d5b88e6adec5426b10c
Bela <sinko_bela@yahoo.com>
2021-03-12 03:43:20 +0200

    .

--- src/main/java/enums/PlanetsMain.java
+++ src/main/java/enums/PlanetsMain.java
@@ -1,6 +1,8 @@
+package enums;
+
 import enums.Planets;
 
-public class Main {
+public class PlanetsMain {
     public static void main(String[] args) {
         for (Planets planet : Planets.values()) {
             System.out.println("Your mass on planet " + planet + " is: " + planet.calculateMass(150));
